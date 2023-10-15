82c4f203620b30c3a74bedcab146ecf13d64e207
angusdq <angusdq@gmail.com>
2023-10-16 01:39:17 +1100

    CSS stylesheet added fix 4

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,3 +1,4 @@
+import '../../src/styles.css'
 import Image from '../components/images/cyclopCharacter.png'
 import React, { useEffect, useRef } from 'react';
 import HighlightFunction from './functions/HighlightFunction';
